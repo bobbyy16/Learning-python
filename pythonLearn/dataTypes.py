0198164eb2cc9270234f6ee26e1a4a78c4617947
bobbyy16 <bobbyyyyy16@gmail.com>
2022-12-30 12:13:32 +0530

    added concatenation of strings

--- pythonLearn/dataTypes.py
+++ pythonLearn/dataTypes.py
@@ -8,4 +8,9 @@ print(200)
 print(21.22)
 
 # Arthmetic operations
-print(20 * 24 + 20)
+print(20 * 24 * 60)
+
+# string concatenation
+
+print("20 days are " + str(28800) + " minutes")
+print(f"20 days are {20 * 24 * 60} minutes")
