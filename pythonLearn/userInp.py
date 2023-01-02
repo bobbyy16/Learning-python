486f7c56479983be6ec99ceb5e6eb2987e96373b
bobbyy16 <bobbyyyyy16@gmail.com>
2023-01-02 11:44:00 +0530

    added userInput file

---
+++ pythonLearn/userInp.py
@@ -0,0 +1,6 @@
+def fun(numberOfDays):
+    return f"{numberOfDays} days are {numberOfDays * 24} hours"
+
+data = int(input("Hey user, enter a number of days and I will convert it to hours\n "))
+convertedValue = fun(data)
+print(convertedValue)
