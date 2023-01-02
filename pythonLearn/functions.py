47692dc91fb3cad4425d684328139b5f3bcb90fd
bobbyy16 <bobbyyyyy16@gmail.com>
2023-01-02 11:25:22 +0530

    added dual parameterized function

--- pythonLearn/functions.py
+++ pythonLearn/functions.py
@@ -13,3 +13,10 @@ def parameters(num_of_days):
     print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
 
 parameters(35)
+
+
+def dual_parameters(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print(custom_message)
+
+dual_parameters(30, "yo")
